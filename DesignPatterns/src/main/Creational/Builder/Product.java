6987e54711b0fcbda51ea01ecf42d24a8d88f8d2
chadkila <yuzhaobo77@gmail.com>
2014-09-06 23:52:01 -0700

    concrete product

---
+++ DesignPatterns/src/main/Creational/Builder/Product.java
@@ -0,0 +1,25 @@
+package main.Creational.Builder;
+
+import main.HelloWorld;
+
+/**
+ * An entity of product, which implements HelloWorld interface and will accept
+ * "Hello World" from the builder
+ * 
+ * @author yu
+ * 
+ */
+public class Product implements HelloWorld {
+
+	private String str;
+
+	public void setString(String s) {
+		this.str = s;
+	}
+
+	@Override
+	public String hellWorld() {
+		return str;
+	}
+
+}
