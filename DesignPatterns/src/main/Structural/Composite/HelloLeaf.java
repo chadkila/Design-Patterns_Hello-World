be1a28830950f6280a64d5ec16dff0fe11c88268
chadkila <yuzhaobo77@gmail.com>
2014-10-27 23:50:45 -0700

    Leaf Component

---
+++ DesignPatterns/src/main/Structural/Composite/HelloLeaf.java
@@ -0,0 +1,30 @@
+/**
+ * http://uule.iteye.com/blog/868224
+ * This works as a leaf component that implements HelloWorld.
+ */
+package main.Structural.Composite;
+
+import main.HelloWorld;
+
+/**
+ * @author yu
+ * 
+ */
+public class HelloLeaf implements HelloWorld {
+
+	String hello;
+
+	public HelloLeaf(String str) {
+		this.hello = str;
+	}
+
+	@Override
+	public String hellWorld() {
+		return hello;
+	}
+
+	public void display() {
+		System.out.print(hello);
+	}
+
+}
